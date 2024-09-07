9745dbf938a76dac59c9f88e91b286bf5adddcab
ringo9971 <ab7usa@gmail.com>
2024-09-07 13:00:43 +0900

    :children_crossing: cat-market でチェックを入れたときに素早く反応させる

--- src/hooks/market/useWishItemsOperation.ts
+++ src/hooks/market/useWishItemsOperation.ts
@@ -77,6 +77,9 @@ const useWishItemsOperation = (): UseWishItemsOperationState => {
       ...wishItem,
       check: !wishItem.check,
     };
+    setWishList((list) =>
+      list.map((l) => (l.id === wishItem.id ? toggledData : l))
+    );
     const res = await apiClient.updateListItem<WishItem>(
       'market',
       'wishList',
