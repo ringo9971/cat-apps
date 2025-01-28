606803aeaf217ac8ece77076edd4ae03100e05a3
ringo9971 <ab7usa@gmail.com>
2025-01-28 13:59:25 +0900

    :bug: Tab ボタンで切り替えれなくなっていた問題の修正

--- src/features/market/routes/MarketPage.tsx
+++ src/features/market/routes/MarketPage.tsx
@@ -2,23 +2,31 @@ import { Box, Tab, Tabs } from '@mui/material';
 import FoodMenuContainer from 'features/market/containers/FoodMenu';
 import ToDoListContainer from 'features/market/containers/ToDoList';
 import WishListContainer from 'features/market/containers/WishList';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
 
 import 'swiper/css';
 
 export const MarketPage = (): JSX.Element => {
   const [tabpage, setTabpage] = useState(0);
+  const swiperRef = useRef<SwiperCore | null>(null);
+
+  const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
+    setTabpage(newValue);
+    swiperRef.current?.slideTo(newValue);
+  };
 
   return (
     <Box>
-      <Tabs value={tabpage} onChange={(_, newValue) => setTabpage(newValue)}>
+      <Tabs value={tabpage} onChange={handleTabChange}>
         <Tab label="買い物" />
         <Tab label="献立" />
         <Tab label="ToDo" />
       </Tabs>
       <Swiper
         onSlideChange={(swiper) => setTabpage(swiper.activeIndex)}
+        onBeforeInit={(swiper) => (swiperRef.current = swiper)}
         initialSlide={tabpage}
         spaceBetween={16}
         slidesPerView={1}
