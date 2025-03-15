ad1e4c63b7f8cf017d96915afaa301c02785f374
ringo9971 <ab7usa@gmail.com>
2025-03-15 17:41:52 +0900

    :recycle: wishList の表示コンポーネントを分ける

--- src/features/market/components/WishList.tsx
+++ src/features/market/components/WishList.tsx
@@ -2,17 +2,17 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { Box, Card, Checkbox, CardContent, IconButton } from '@mui/material';
 import { WishItem } from 'types/market/WishItem';
 
-type WishListProps = {
-  wishList: Array<WishItem>;
+type WishItemCardProps = {
+  wishItem: WishItem;
   onDelete: (wishItem: WishItem) => void;
   onCheck: (wishItem: WishItem) => void;
 };
 
-const WishList = ({
-  wishList,
+const WishItemCard = ({
+  wishItem,
   onDelete,
   onCheck,
-}: WishListProps): JSX.Element => {
+}: WishItemCardProps): JSX.Element => {
   const getTagColor = (tag: string) => {
     switch (tag) {
       case '食品':
@@ -27,32 +27,49 @@ const WishList = ({
   };
 
   return (
+    <Card
+      key={JSON.stringify(wishItem)}
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      <Box
+        sx={{
+          width: 16,
+          backgroundColor: getTagColor(wishItem.tag),
+        }}
+      />
+      <CardContent>{wishItem.name}</CardContent>
+      <Box display="flex" justifyContent="flex-end" sx={{ flexGrow: 1 }}>
+        <Checkbox checked={wishItem.check} onClick={() => onCheck(wishItem)} />
+        <IconButton onClick={() => onDelete(wishItem)}>
+          <DeleteIcon />
+        </IconButton>
+      </Box>
+    </Card>
+  );
+};
+
+type WishListProps = {
+  wishList: Array<WishItem>;
+  onDelete: (wishItem: WishItem) => void;
+  onCheck: (wishItem: WishItem) => void;
+};
+
+const WishList = ({
+  wishList,
+  onDelete,
+  onCheck,
+}: WishListProps): JSX.Element => {
+  return (
     <Box>
       {wishList.map((wishItem) => (
-        <Card
-          key={JSON.stringify(wishItem)}
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-          }}
-        >
-          <Box
-            sx={{
-              width: 16,
-              backgroundColor: getTagColor(wishItem.tag),
-            }}
-          />
-          <CardContent>{wishItem.name}</CardContent>
-          <Box display="flex" justifyContent="flex-end" sx={{ flexGrow: 1 }}>
-            <Checkbox
-              checked={wishItem.check}
-              onClick={() => onCheck(wishItem)}
-            />
-            <IconButton onClick={() => onDelete(wishItem)}>
-              <DeleteIcon />
-            </IconButton>
-          </Box>
-        </Card>
+        <WishItemCard
+          wishItem={wishItem}
+          onDelete={onDelete}
+          onCheck={onCheck}
+        />
       ))}
     </Box>
   );
