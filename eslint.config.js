73d2c4af9351da9128c54afded1f2dc1e4cb643f
ringo9971 <ab7usa@gmail.com>
2025-11-05 20:54:52 +0900

    :wrench: formatter を prettier に

--- eslint.config.js
+++ eslint.config.js
@@ -1,83 +1,63 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import importPlugin from "eslint-plugin-import";
-import globals from "globals";
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
   {
-    ignores: ["dist", ".eslintrc.cjs"],
+    ignores: ['dist', '.eslintrc.cjs'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
       },
       globals: {
         ...globals.browser,
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-        React: "writable",
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        React: 'writable',
       },
     },
     plugins: {
       react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       import: importPlugin,
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
         },
       ],
-      "react-refresh/only-export-components": [
-        "warn",
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
-
-      "import/order": [
-        "warn",
-        {
-          groups: [
-            ["builtin", "external"],
-            "internal",
-            ["parent", "sibling", "index", "object", "type"],
-          ],
-          "newlines-between": "always",
-          pathGroupsExcludedImportTypes: ["builtin"],
-          alphabetize: { order: "asc", caseInsensitive: true },
-          pathGroups: [
-            { pattern: "src/types/**", group: "internal", position: "before" },
-            {
-              pattern: "src/repositories/**",
-              group: "internal",
-              position: "before",
-            },
-          ],
-        },
-      ],
-      "import/no-relative-parent-imports": "warn",
+      'import/order': 'off',
+      'import/no-relative-parent-imports': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ];
